3265252ea167ed2e26b398e155c14d64b89f3de4
Gganzarolli <86580020+Gganzarolli@users.noreply.github.com>
2026-09-01 09:40:58 -0300

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "discursantes-v6";
+const CACHE_NAME = "discursantes-v7";
 const APP_SHELL = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
